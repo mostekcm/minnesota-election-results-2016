40b69a139d47313e2045891911feec0d4979ae1a
Carlos Mostek <carlos.mostek@auth0.com>
2016-11-05 23:33:33 -0500

    move backend to its own app

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-var backend = require('./api/server');
-backend.set('port', process.env.BACKEND_PORT || 3001);
-
-backend.listen(backend.get('port'), function() {
-    console.log('Backend server listening on port ' + backend.get('port') + ' in ' + backend.get('env') + ' mode');
-});
-
 var frontend = require('./client/client');
 frontend.set('port', process.env.PORT || 3000);
 
